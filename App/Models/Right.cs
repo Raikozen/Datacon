799b2dbcaeacc12ba34cad2ba9222f31953f0b1a
Maarten Dekker <m.p.dekker@zeelandnet.nl>
2018-03-28 11:22:56 +0200

    work in progress

---
+++ App/Models/Right.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Right
+    {
+        public int id { get; }
+        public string name { get; }
+
+        public Right(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
