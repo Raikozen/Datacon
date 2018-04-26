36aa5cedad36010e02c9e992e8e746f455e69f55
Jip Veeke <jipveeke@gmail.com>
2018-04-26 09:36:27 +0200

    newsfeed start

---
+++ App/Models/NewsFeedPost.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class NewsfeedPost
+    {
+		private DateTime date;
+
+		public int Id { get; }
+		public string Message { get; }
+		public string Date
+		{
+			get
+			{
+				int day = date.Day;
+				int month = date.Month;
+				int year = date.Year;
+				string result = $"{day}-{month}-{year}";
+
+				return result;
+			}
+		}
+
+		public NewsfeedPost(int id, string message, DateTime date)
+		{
+			Id = id;
+			Message = message;
+			this.date = date;
+		}
+	}
+}
