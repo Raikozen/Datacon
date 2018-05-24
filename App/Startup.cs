2155adf386935aa91f68f987ba4497ddf1d23f0b
Jip Veeke <jipveeke@gmail.com>
2018-05-24 10:46:50 +0200

    added hanging fire

--- App/Startup.cs
+++ App/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Hangfire;
 
 namespace App
 {
@@ -21,9 +22,13 @@ namespace App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Config for the use of sessions within the application.
+			//Config for the use of sessions within the application.
 			services.AddMvc().AddSessionStateTempDataProvider();
             services.AddSession();
+
+			services.AddHangfire(config => {
+				config.UseSqlServerStorage(Configuration.GetConnectionString("Default"));
+			});
 		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,8 +44,11 @@ namespace App
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseStaticFiles();
+			app.UseStaticFiles();
             app.UseSession();
+			app.UseHangfireDashboard();
+			app.UseHangfireServer();
+
 			app.UseMvc(routes =>
             {
                 routes.MapRoute(
